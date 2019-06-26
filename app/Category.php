4bdc8afffd882bdee8eb51edbe05dea6d54df98e
Daniel Plewinski <dplewinski@gmail.com>
2019-06-26 10:49:47 +0200

    Initial commit

---
+++ app/Category.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+/**
+ * Class Category
+ * @package App
+ */
+class Category extends Model
+{
+    protected $fillable = ['name'];
+}
